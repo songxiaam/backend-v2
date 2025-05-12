5c963cf0a48c424a9ae402ce21fcb0ed6b2e349a
lc3091 <309138912@qq.com>
2025-05-12 18:45:13 +0800

    comment rpc client and etcd

--- app/api/internal/config/config.go
+++ app/api/internal/config/config.go
@@ -2,13 +2,12 @@ package config
 
 import (
 	"github.com/zeromicro/go-zero/rest"
-	"github.com/zeromicro/go-zero/zrpc"
 )
 
 type Config struct {
 	rest.RestConf
-	RPCClientConf zrpc.RpcClientConf
-	DB            struct {
+	// RPCClientConf zrpc.RpcClientConf
+	DB struct {
 		DataSource string
 	}
 	Auth struct {
--- app/api/internal/svc/serviceContext.go
+++ app/api/internal/svc/serviceContext.go
@@ -3,11 +3,9 @@ package svc
 import (
 	"github.com/zeromicro/go-zero/core/stores/sqlx"
 	"github.com/zeromicro/go-zero/rest"
-	"github.com/zeromicro/go-zero/zrpc"
 	"metaLand/app/api/internal/config"
 	"metaLand/app/api/internal/middleware"
 	"metaLand/app/api/model"
-	"metaLand/app/worker/metalandworker"
 )
 
 type ServiceContext struct {
@@ -15,7 +13,7 @@ type ServiceContext struct {
 	OIDCAuthMiddleware rest.Middleware
 	ChainModel         model.ChainModel
 	StartupModel       model.StartupModel
-	RPCClient          metalandworker.MetaLandWorker
+	// RPCClient          metalandworker.MetaLandWorker
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
@@ -24,6 +22,6 @@ func NewServiceContext(c config.Config) *ServiceContext {
 		OIDCAuthMiddleware: middleware.NewOIDCAuthMiddleware().Handle,
 		ChainModel:         model.NewChainModel(sqlx.NewMysql(c.DB.DataSource)),
 		StartupModel:       model.NewStartupModel(sqlx.NewMysql(c.DB.DataSource)),
-		RPCClient:          metalandworker.NewMetaLandWorker(zrpc.MustNewClient(c.RPCClientConf)),
+		// RPCClient:          metalandworker.NewMetaLandWorker(zrpc.MustNewClient(c.RPCClientConf)),
 	}
 }
